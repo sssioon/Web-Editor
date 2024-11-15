3c6d09510ec3bf5eb8e7e730a782f06927776b7e
sssioon <sssioon@hotmail.com>
2024-11-15 12:37:11 +0800

    Update cors-proxy.js

--- api/cors-proxy.js
+++ api/cors-proxy.js
@@ -1,1 +1,19 @@
+// cors-proxy.js
+module.exports = async (req, res) => {
+  const url = req.query.url;
+  
+  if (!url) {
+    return res.status(400).send('Missing "url" query parameter');
+  }
 
+  try {
+    const response = await fetch(url);
+    const body = await response.text();
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send(body);
+  } catch (error) {
+    res.status(500).send('Error fetching URL');
+  }
+};
